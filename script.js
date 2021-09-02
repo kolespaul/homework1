64dbe7b971a1e3a2a7d66da57c9ee64aaf147e2c
Pavel <koles.paul@gmail.com>
2021-09-02 18:11:17 +0300

    version_1.1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let a = parseInt(prompt('Number one', ''));
 let b = parseInt(prompt('Number two', ''));
 
-if (b < 2 || b > 36 || b == null || isNaN(b)){
+if (b < 2 || b > 36 || isNaN(a) || isNaN(b)){
     console.log('Некорректный ввод!');
 } else {
     let c = a.toString(b);
@@ -14,15 +14,20 @@ if (b < 2 || b > 36 || b == null || isNaN(b)){
 // 2) Task #2
 
 let d = parseInt(prompt('Number one', ''));
-let e = parseInt(prompt('Number two', ''));
 
-if (d == null || e == null || isNaN(d) || isNaN(e)) {
+if (isNaN(d)) {
     console.log('Некорректный ввод!');
 } else {
-    let f = d + e;
-    let g = d / e;
-    console.log(`Ответ: ${f}, ${g}.`);
+    let e = parseInt(prompt('Number two', ''));
+
+    if (isNaN(e)) {
+        console.log('Некорректный ввод!');
+    } else {
+        let f = d + e;
+        let g = d / e;
+        console.log(`Ответ: ${f}, ${g}.`);
+    }
 }
 
 
-// Вопрос: может ли быть случай, когда нам нужно делать проверку "n == null"?
+
